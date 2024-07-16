be975146ab871f010b5fb7b8937f7899400398f6
dilyara4949 <mukhambetovad49@mail.ru>
2024-07-16 14:04:38 +0500

    feat: add get flight api

---
+++ internal/handler/flight.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"context"
+	"github.com/dilyara4949/flight-booking-api/internal/domain"
+	"github.com/dilyara4949/flight-booking-api/internal/handler/response"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+type FlightService interface {
+	Get(ctx context.Context, id uuid.UUID) (*domain.Flight, error)
+}
+
+func GetFlightHandler(service FlightService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		flightID, err := uuid.Parse(c.Param("flightId"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, response.Error{Error: "id format is not correct"})
+
+			return
+		}
+
+		flight, err := service.Get(c, flightID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, response.Error{Error: err.Error()})
+
+			return
+		}
+		c.JSON(http.StatusOK, flight)
+	}
+}
