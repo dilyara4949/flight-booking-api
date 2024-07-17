36cc3a97355d879926a934eec2bebb727ba2e746
dilyara4949 <mukhambetovad49@mail.ru>
2024-07-17 13:16:53 +0500

    feat: add repo, service, routes to api

---
+++ internal/repository/flight.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/dilyara4949/flight-booking-api/internal/domain"
+	"gorm.io/gorm"
+)
+
+type FlightRepository struct {
+	db *gorm.DB
+}
+
+func NewFlightRepository(db *gorm.DB) FlightRepository {
+	return FlightRepository{db: db}
+}
+
+func (repo *FlightRepository) GetAll(ctx context.Context, page, pageSize int) ([]domain.Flight, error) {
+	var flights []domain.Flight
+
+	offset := (page - 1) * pageSize
+
+	if err := repo.db.WithContext(ctx).Limit(pageSize).Offset(offset).Find(&flights).Error; err != nil {
+		return nil, fmt.Errorf("get all flights error: %w", err)
+	}
+	return flights, nil
+}
---
+++ internal/service/flight.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+	"github.com/dilyara4949/flight-booking-api/internal/domain"
+	"github.com/dilyara4949/flight-booking-api/internal/repository"
+)
+
+type Flight struct {
+	repo repository.FlightRepository
+}
+
+func NewFlightService(repo repository.FlightRepository) *Flight {
+	return &Flight{repo: repo}
+}
+
+func (service *Flight) GetAll(ctx context.Context, page, pageSize int) ([]domain.Flight, error) {
+	return service.repo.GetAll(ctx, page, pageSize)
+}
