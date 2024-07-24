c9469f9153139a83703904710d6533c2e26670e0
dilyara4949 <mukhambetovad49@mail.ru>
2024-07-24 11:32:10 +0500

    tests: add test for signin handler

--- internal/handler/auth_test.go
+++ internal/handler/auth_test.go
@@ -44,7 +44,10 @@ func (s userServiceMock) CreateUser(ctx context.Context, signup request.Signup, 
 }
 
 func (s userServiceMock) ValidateUser(ctx context.Context, signin request.Signin) (domain.User, error) {
-	return domain.User{}, nil
+	if s.err != nil {
+		return domain.User{}, s.err
+	}
+	return s.user, nil
 }
 
 func TestSignupHandler(t *testing.T) {
@@ -55,7 +58,7 @@ func TestSignupHandler(t *testing.T) {
 		expectedCode int
 		expectedBody string
 	}{
-		"Valid signup": {
+		"OK": {
 			body: `{
 				"email": "test@example.com",
 				"password": "password",
@@ -74,16 +77,15 @@ func TestSignupHandler(t *testing.T) {
 			expectedCode: http.StatusOK,
 			expectedBody: `{"access_token":"token123","user":{"id":"%s","email":"test@example.com","phone":"","created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z"}}`,
 		},
-		"Invalid request body": {
+		"invalid request body": {
 			body:         `{}`,
 			expectedCode: http.StatusBadRequest,
 			expectedBody: `{"error":"fields cannot be empty"}`,
 		},
-		"Error creating user": {
+		"error creating user": {
 			body: `{
 				"email": "test@example.com",
-				"password": "password",
-				"role": "user"
+				"password": "password"
 			}`,
 			userService: userServiceMock{
 				err: errors.New("error creating user"),
@@ -94,14 +96,12 @@ func TestSignupHandler(t *testing.T) {
 		"Error creating token": {
 			body: `{
 				"email": "test@example.com",
-				"password": "password",
-				"role": "user"
+				"password": "password"
 			}`,
 			userService: userServiceMock{
 				user: domain.User{
 					ID:    uuid.New(),
 					Email: "test@example.com",
-					Role:  "user",
 				},
 			},
 			authService: authServiceMock{
@@ -148,6 +148,120 @@ func TestSignupHandler(t *testing.T) {
 				expectedBody := strings.Replace(tt.expectedBody, "%s", tt.userService.user.ID.String(), 1)
 				var expected, actual map[string]interface{}
 				if err = json.Unmarshal([]byte(expectedBody), &expected); err != nil {
+					t.Fatalf("couldn't unmarshal expected response: %v", err)
+				}
+				if err = json.Unmarshal(body, &actual); err != nil {
+					t.Fatalf("couldn't unmarshal actual response: %v", err)
+				}
+
+				if !reflect.DeepEqual(expected, actual) {
+					t.Errorf("expected body %v, got %v", expected, actual)
+				}
+			} else {
+				if string(body) != tt.expectedBody {
+					t.Errorf("expected body %s, got %s", tt.expectedBody, body)
+				}
+			}
+		})
+	}
+}
+
+func TestSigninHandler(t *testing.T) {
+	tests := map[string]struct {
+		body         string
+		userService  userServiceMock
+		authService  authServiceMock
+		expectedCode int
+		expectedBody string
+	}{
+		"OK": {
+			body: `{
+				"email": "test@example.com",
+				"password": "password"
+			}`,
+			userService: userServiceMock{
+				user: domain.User{
+					ID:    uuid.New(),
+					Email: "test@example.com",
+				},
+			},
+			authService: authServiceMock{
+				token: "token123",
+			},
+			expectedCode: http.StatusOK,
+			expectedBody: `{"access_token":"token123"}`,
+		},
+		"Invalid request body": {
+			body:         `{}`,
+			expectedCode: http.StatusBadRequest,
+			expectedBody: `{"error":"fields cannot be empty"}`,
+		},
+		"Error validating user": {
+			body: `{
+				"email": "test@example.com",
+				"password": "password"
+			}`,
+			userService: userServiceMock{
+				err: errors.New("error validating user"),
+			},
+			expectedCode: http.StatusNotFound,
+			expectedBody: `{"error":"invalid email or password"}`,
+		},
+		"Error creating token": {
+			body: `{
+				"email": "test@example.com",
+				"password": "password"
+			}`,
+			userService: userServiceMock{
+				user: domain.User{
+					ID:    uuid.New(),
+					Email: "test@example.com",
+					Role:  "user",
+				},
+			},
+			authService: authServiceMock{
+				err: errors.New("error creating token"),
+			},
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: `{"error":"create access token error"}`,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			gin.SetMode(gin.TestMode)
+			r := gin.New()
+
+			cfg := config.Config{
+				JWTTokenSecret:    "secret",
+				AccessTokenExpire: 3600,
+			}
+
+			r.POST("/signin", SigninHandler(tt.authService, tt.userService, cfg))
+
+			req, err := http.NewRequest(http.MethodPost, "/signin", strings.NewReader(tt.body))
+			if err != nil {
+				t.Fatalf("couldn't create request: %v", err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			resp := w.Result()
+			if resp.StatusCode != tt.expectedCode {
+				t.Errorf("expected status %d, got %d", tt.expectedCode, resp.StatusCode)
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("couldn't read response body: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if tt.expectedCode == http.StatusOK {
+				var expected, actual map[string]interface{}
+				if err = json.Unmarshal([]byte(tt.expectedBody), &expected); err != nil {
 					t.Fatalf("couldn't unmarshal expected response: %v", err)
 				}
 				if err = json.Unmarshal(body, &actual); err != nil {
