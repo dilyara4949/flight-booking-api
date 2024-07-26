92130fe2593bc2974095a5a0178fc21b9a843b4c
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2024-07-26 11:39:15 +0500

    Feature/25 delete flight api (#29)
    * feat: add delete flight handler
    
    * feat: add repo, service
    
    * fix: add middlewares;

--- internal/repository/errors/errors.go
+++ internal/repository/errors/errors.go
@@ -3,6 +3,7 @@ package errors
 import "errors"
 
 var (
+	ErrFlightNotFound       = errors.New("flight not found")
 	ErrUserNotFound         = errors.New("user not found")
 	ErrInvalidEmailPassword = errors.New("invalid email or password")
 )
