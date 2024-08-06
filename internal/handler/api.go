47af676b3344553f964aec8d3707815af2189cb9
dilyara4949 <mukhambetovad49@mail.ru>
2024-08-06 12:22:36 +0500

    fix: use const instead of the string

--- internal/handler/api.go
+++ internal/handler/api.go
@@ -34,7 +34,7 @@ func NewAPI(cfg config.Config, database *gorm.DB) *gin.Engine {
 			}
 			users := v1.Group("/users")
 			{
-				admin := users.Use(middleware.JWTAuth(cfg.JWTTokenSecret), middleware.AccessCheck("admin"))
+				admin := users.Use(middleware.JWTAuth(cfg.JWTTokenSecret), middleware.AccessCheck(AdminRole))
 				{
 					admin.GET("/", GetUsersHandler(userService))
 				}
@@ -53,7 +53,7 @@ func NewAPI(cfg config.Config, database *gorm.DB) *gin.Engine {
 					private.GET("/:flightId", GetFlightHandler(flightService))
 					private.GET("/", GetFlights(flightService))
 				}
-				admin := flights.Use(middleware.JWTAuth(cfg.JWTTokenSecret), middleware.AccessCheck("admin"))
+				admin := flights.Use(middleware.JWTAuth(cfg.JWTTokenSecret), middleware.AccessCheck(AdminRole))
 				{
 					admin.POST("/", CreateFlightHandler(flightService))
 					admin.DELETE("/:flightId", DeleteFlightHandler(flightService))
